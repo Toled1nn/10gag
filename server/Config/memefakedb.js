86d31b51c758c6dc59895ffc4d2fb8c15fd9e9c2
Toled1nn <luiztoledo@id.uff.br>
2021-04-22 18:15:50 -0300

    alteração meme

--- server/Config/memefakedb.js
+++ server/Config/memefakedb.js
@@ -8,7 +8,7 @@ const Memes = [
     },
     {
         id: '02',
-        title: "Chapolin cu de fossa",
+        title: "Chapolin de fossa",
         desc: "Chapolin cu de ralo comendo cu de curioso",
         img: "https://www.sescsp.org.br/files/artigo/8e93bcd9/d433/4a19/a8ac/4244df55c1fe.jpg",
         
