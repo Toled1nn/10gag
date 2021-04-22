de8acaa2ed1fa600a2cf15aa5864c3153c4a2e6e
Toled1nn <luiztoledo@id.uff.br>
2021-04-22 18:58:36 -0300

    Adicionar memes

--- server/Config/memefakedb.js
+++ server/Config/memefakedb.js
@@ -1,17 +1,41 @@
 const Memes = [
     {
         id: '01',
-        title: "Willy Wonka Cracudo",
-        desc: "rara visao do willy wonka crackudao",
-        img: "https://img.ibxk.com.br/2019/07/16/16123623485057.jpg",
+        title: "A melhor invenção",
+        desc: "Genial",
+        img: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t1.6435-9/177565176_1253486698465330_6184829210043265913_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=9267fe&_nc_eui2=AeFTflaRGN9lvw8bI-FVJ_r5KgZ07a-MQc0qBnTtr4xBzd1Ujv4c4EHBIm6ORrDI6G6wblv0_fc3W3Tlk5PS7Ktm&_nc_ohc=6oF94HmXNoIAX_B8Ggh&_nc_ht=scontent.fbsb3-1.fna&oh=03d6244e14de090edfcfdd560e07b458&oe=60A76C84",
+        
+    },
+   
+    {
+        id: '02',
+        title: "Lembrete diário para ajustar a postura",
+        desc: "É triste",
+        img: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t1.6435-9/176368103_10159025951251585_553114042659049512_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=2c4854&_nc_eui2=AeH_Wq11aFT0yeF_C4MCm98XbdGil54makVt0aKXniZqRVE-ZtM-7DDdrSI9hOzhV1cyMAZXuuRtQafCcE0UGzlh&_nc_ohc=sA1D9bOivyAAX-vZBzK&_nc_ht=scontent.fbsb3-1.fna&oh=d44b4f5f4f23571c6d39386c4666cdca&oe=60A73787",
+        
+    },
+   
+    {
+        id: '03',
+        title: "A parte mais dolorosa dos jogos",
+        desc: "RIP Yoshi",
+        img: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t1.6435-9/174670352_498037911560074_5752760404862337495_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeHTKG5qcS-OWgT3X-AEJ7LLsWJbziYIdJyxYlvOJgh0nIp0cyIGBuF_Tl3RHSujfypBkQsyjV50n8qgb8jDRl9E&_nc_ohc=N3iB3YKKIoAAX9FdKh7&_nc_ht=scontent.fbsb3-1.fna&oh=3bbcdfdcb8d52e960a12a50b12184813&oe=60A6F557",
         
     },
     {
-        id: '02',
-        title: "Chapolin de fossa",
-        desc: "Chapolin cu de ralo comendo cu de curioso",
-        img: "https://www.sescsp.org.br/files/artigo/8e93bcd9/d433/4a19/a8ac/4244df55c1fe.jpg",
+        id: '04',
+        title: "Deadpool resolve tudo",
+        desc: "Spider-pó",
+        img: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t1.6435-9/174322066_5637651546307766_2777914109919935118_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeFS5sX4TEIeqvM3v0xITnki5gGXQ2frtPPmAZdDZ-u085xOjDRwjbDEXnfzKWIIQm0j5wgqgTqIvyq4MnXchYEJ&_nc_ohc=7Jo136J260UAX8IKu_a&_nc_ht=scontent.fbsb3-1.fna&oh=15a74d17b830a985096b77884810e3df&oe=60A8A8C3",
         
-    }
+    },
+    {
+        id: '05',
+        title: "Stonks",
+        desc: "O famoso copo meio cheio",
+        img: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t1.6435-9/173137277_10161529712396840_1539881695110884992_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeH0wc4xFpA39w6ALUmNU7gjVAnGrsvao-JUCcauy9qj4oItTKyYJm3Qw3jUKQaUmB9mAlq7fo0Ro9XP2PAiNB1I&_nc_ohc=33jn8DCiZMwAX9F4nTU&_nc_ht=scontent.fbsb3-1.fna&oh=c233cd397c57d5bf4e1873c706454a9a&oe=60A6A0F2",
+        
+    },
  ]
+
 module.exports = Memes;
